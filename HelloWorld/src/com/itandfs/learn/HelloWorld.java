d2c4d4d45fc8263d6b6865e1773a2a28761fbf67
Kiran Gangireddyvari <kiran.gangireddyvari@gmail.com>
2017-02-25 07:31:46 -0500

    First Project

---
+++ HelloWorld/src/com/itandfs/learn/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itandfs.learn;
+
+/**
+ * Created by kiran on 2/25/2017.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
